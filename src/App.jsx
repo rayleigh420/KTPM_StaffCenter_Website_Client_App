f1575ef13c373c431c8a1ab9b03a2e2090fce37c
rayleigh420 <nhatduy0409@gmail.com>
2023-08-11 10:57:09 +0700

    Config pretier

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
 
 function App() {
-
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<h1 className='text-red-500'>Hello World Duy Le</h1>} />
-        <Route path='/test' element={<button className='btn'>Button</button>} />
-      </Routes>
-    </>
-  )
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<h1 className="text-red-500">Hello World Duy Le</h1>} />
+				<Route path="/test" element={<button className="btn">Button</button>} />
+			</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false
-    }
-  }
-})
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
-)
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+            ,
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+);
