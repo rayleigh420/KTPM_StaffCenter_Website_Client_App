b85342d0ec474b43b85ed51c18d4efff8de60ec4
rayleigh420 <nhatduy0409@gmail.com>
2023-08-11 10:45:02 +0700

    Set up react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-red-500'>Hello World Duy Le</h1>
-	  <button className="btn">Button</button>
+      <Routes>
+        <Route path='/' element={<h1 className='text-red-500'>Hello World Duy Le</h1>} />
+        <Route path='/test' element={<button className='btn'>Button</button>} />
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { BrowserRouter } from 'react-router-dom'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 )
