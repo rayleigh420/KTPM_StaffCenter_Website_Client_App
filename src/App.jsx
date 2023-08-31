2c4ce1cee8ef7b2fb5d81ead3823e6132547efda
rayleigh420 <nhatduy0409@gmail.com>
2023-08-31 21:12:19 +0700

    Fix some routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import DetailLocate from './components/DetailLocate/DetailLocate';
 import Header from './components/Header/Header';
 import Locate from './components/Locate/Locate';
 import Receive from './components/Receive/Receive';
-import Status from './components/Status/Status'
+import Status from './components/Status/Status';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Route, Link, Router, Routes } from 'react-router-dom';
 
