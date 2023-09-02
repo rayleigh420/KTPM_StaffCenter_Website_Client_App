3d6cae00626f1088dbfd5e8f3d202c948ec01885
rayleigh420 <nhatduy0409@gmail.com>
2023-09-02 20:16:15 +0700

    API for get history

--- src/apis/bookAPI.js
+++ src/apis/bookAPI.js
@@ -4,3 +4,8 @@ export const bookDirect = async (data) => {
     const result = await axios.post('/booking/request-ride', data)
     return result.data
 }
+
+export const getHistory = async (phone) => {
+    const result = await axios.post('booking/getLocation')
+    return result.data
+}
